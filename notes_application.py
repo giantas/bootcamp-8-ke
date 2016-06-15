bd54d3fa8ec32fb45a79dbf356e233f1eddd0bb2
Aswa Paul <aswapaul@gmail.com>
2016-06-15 19:18:13 +0300

    Fixed edit error. Made other minor changes

--- notes_application.py
+++ notes_application.py
@@ -26,11 +26,12 @@ class NotesApplication(object):
         return self.notes[note_id]
       
     def search(self, search_text):
-        print "Showing results for search '%s': \n" % search_text
+        print "Showing results for search '%s': " % search_text
         for note in self.notes:
             if search_text in note:
                 print "\nNote ID: %s" % self.notes.index(note)
-                print note,"\n"
+                print note
+                print "By Author: %s" % self.author
         """
         if not_found: 
             print "Found 0 matches"
@@ -46,7 +47,7 @@ def main():
     auth_name = raw_input("Enter note author: ")
     note1 = NotesApplication(auth_name)
     
-    choice = raw_input("\nSelect you preferred option: \n\t1. Add note\n\t2. Edit note\n\t3. Print notes\n\t4. Exit\nSelection: ")
+    choice = raw_input("\nSelect you preferred option: \n\t1. Add note\n\t2. Search string\n\t3. Edit note\n\t4. Print notes\n\t5. Exit\nSelection: ")
     
     while choice is not '4':
         if choice == '1':
@@ -54,17 +55,28 @@ def main():
             note1.create(new_note)
             print "Note added!\n"
         elif choice == '2':
-            edit = raw_input('Enter the search string: ')
-            note1.search(edit)
+            search_string = raw_input('Enter the search string: ')
+            note1.search(search_string)
         elif choice == '3':
+            if note1.num_of_notes() == 0:
+                print "\nAdd note first!"
+            else:
+                id_input = raw_input("Enter Note ID: ")
+                note_edit = raw_input("Enter note content: ")
+                note1.edit(id_input,note_edit)
+                print "Note edited!"
+        elif choice == '4':
             if note1.num_of_notes() == 0:
                 print "No notes found here"
             else:
                 note1.list()
-        elif choice == '4':
+        elif choice == '5':
+            print "Goodbye!"
             exit()
+        else:
+            print "Incorrect selection. (use the numbers)"
             
-        choice = raw_input("Choose you preferred option: \n\t1. Add note\n\t2. Edit note\n\t3. Print notes\nSelection:")
+        choice = raw_input("\nSelect you preferred option: \n\t1. Add note\n\t2. Search string\n\t3. Edit note\n\t4. Print notes\n\t5. Exit\nSelection: ")
         
     
 if __name__ == "__main__":
