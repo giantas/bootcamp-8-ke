4f2ca91bcb22022ecd69df2393bc485778838d10
Aswa Paul <aswapaul@gmail.com>
2016-06-15 11:42:29 +0300

    First commit that tests basic functionality of fizzbuzz

---
+++ test_fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+#encoding: UTF-8
+
+import unittest
+import fizzbuzz
+
+class FizzBuzzTest(unittest.TestCase):
+    """Testing the fizzbuzz
+    """
+    def test_returns_fizz_when_divisible_by_three(self):
+        """Test return fizz when input is 
+        divisible by three
+        """
+        self.assertEqual(fizzbuzz.fizz_buzz(3), "fizz")
+        
+    def test_returns_buzz_when_divisible_by_three(self):
+        """Test return fizz when input is 
+        divisible by three
+        """
+        self.assertEqual(fizzbuzz.fizz_buzz(5), "buzz")
+        
