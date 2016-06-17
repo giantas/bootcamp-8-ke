7acbe70558c0f310f4e783c4acfaab612debd1bc
Aswa Paul <aswapaul@gmail.com>
2016-06-17 09:44:17 +0300

    Add more tests

--- notes_app_test.py
+++ notes_app_test.py
@@ -15,6 +15,21 @@ class  NotesAppTestCase(unittest.TestCase):
     
     def test_num_of_notes(self):
         self.assertEqual(self.note.num_of_notes(), 0)
+    
+    def test_note_creation(self):
+        self.note.create("This is a test value")
+        self.assertIsNotNone(self.note.num_of_notes())
+    
+    def test_get_note_content_by_id(self):
+        self.note.create("This is a test value")
+        self.assertEqual(self.note.get(0), "This is a test value")
+    
+    def test_note_editing(self):
+        self.note.create("This is a test value")
+        first_value = self.note.get(0)
+        self.note.edit(0,"Value has changed")
+        second_value = self.note.get(0)
+        self.assertNotEqual(first_value, second_value)
 
 
 if __name__ == '__main__':
